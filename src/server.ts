4a30fb399242db8c7090888e15713d3cf2a55924
Henrique Branco <henriquebrancodasilvadias@yahoo.com>
2026-06-18 18:13:54 -0300

    feat: add server setup and initial routes
    - create the application server
    - add initial routes in the src directory

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,27 @@
-console.log("hello world");
+import fastify from "fastify"
+import { request } from "http"
+
+const server = fastify( {logger: true} )
+
+server.get('/grupos', async(request, response) => {
+    response.type('aplication/json').code(200)
+
+    return [
+        {grupo: 'A', Times: 'Brasil, México, Holando, Romenia'}
+    ]
+})
+
+const artilheiros = [
+    {Seleção: "Brasil", Artilheiro:"Neymar"},
+    {Seleção: "Argentina", Artilheiro:"Messi"},
+    {Seleção: "Portugal", Artilheiro:"Cristiano Ronaldo"}
+]
+
+server.get('/artilheiros', async(request, response) => {
+    response.type('aplication/json').code(200)
+    return {artilheiros}
+})
+
+server.listen({port: 3636}, () => {
+    console.log(`Server inciado na porta 3636`)
+})

3f3de4136b7f1e6d0260e1e80d0af100fa3181dc
Henrique Branco <henriquebrancodasilvadias@yahoo.com>
2026-06-19 17:57:40 -0300

    test: add required URL parameter validation tests

--- src/server.ts
+++ src/server.ts
@@ -12,14 +12,25 @@ server.get('/grupos', async(request, response) => {
 })
 
 const artilheiros = [
-    {Seleção: "Brasil", Artilheiro:"Neymar"},
-    {Seleção: "Argentina", Artilheiro:"Messi"},
-    {Seleção: "Portugal", Artilheiro:"Cristiano Ronaldo"}
+    {id: 1, Seleção: "Brasil", Artilheiro:"Neymar"},
+    {id: 2, Seleção: "Argentina", Artilheiro:"Messi"},
+    {id: 3, Seleção: "Portugal", Artilheiro:"Cristiano Ronaldo"}
 ]
 
 server.get('/artilheiros', async(request, response) => {
     response.type('aplication/json').code(200)
     return {artilheiros}
+})
+
+interface RouteParams{
+    id: string
+}
+
+server.get<{Params: RouteParams}>('/artilheiros/:id', async(request, response) => {
+    const id = parseInt(request.params.id)
+    const selecao = artilheiros.find( s => s.id === id)
+
+
 })
 
 server.listen({port: 3636}, () => {
